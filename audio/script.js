54a8d1f60f30a2f39c9e078a6ffd507bedd12c4c
unknown <icw82@yandex.ru>
2014-03-29 13:59:35 +0600

    progress bar and abort active dowload

--- audio/script.js
+++ audio/script.js
@@ -19,19 +19,54 @@ function stopEvent(event){
     return false;
 }
 
-function save(url, name){
-    var xhr = new XMLHttpRequest();
+function save(url, name, element){
     (name) || (name = 'kenzo-vk-audio.mp3');
+
+    var
+        xhr = new XMLHttpRequest(),
+        progress = 0,
+        abort = false,
+        DOM_kz__btn =
+            element.querySelector('.kz-vk-audio__btn'),
+        DOM_kz__progress =
+            element.querySelector('.kz-vk-audio__progress'),
+        DOM_kz__progress_filling =
+            element.querySelector('.kz-vk-audio__progress-filling');
+
+    function show_progress_bar(){
+        if (!DOM_kz__btn.classList.contains('kz-hidden'))
+            DOM_kz__btn.classList.add('kz-hidden');
+
+        DOM_kz__progress.classList.remove('kz-hidden');
+    }
+
+    function hide_progress_bar(){
+        if (!DOM_kz__progress.classList.contains('kz-hidden'))
+            DOM_kz__progress.classList.add('kz-hidden');
+
+        DOM_kz__btn.classList.remove('kz-hidden');
+    }
+
+    DOM_kz__progress.addEventListener('click', function(event){
+        stopEvent(event);
+        xhr.abort();
+        abort = true;
+        hide_progress_bar();
+    }, false);
 
     xhr.responseType = 'blob';
     xhr.onprogress = function(progress){
-        if (progress.lengthComputable){
-            console.log(progress.loaded + ' / ' + progress.total);
+        if (progress.lengthComputable && !abort){
+            show_progress_bar();
+            progress = Math.floor(progress.loaded / progress.total * 100);
+            DOM_kz__progress_filling.style.left = -100 + progress + '%';
+            DOM_kz__progress.setAttribute('data-progress', progress + '%');
         }
     }
     xhr.onload = function(){
         var blob = new window.Blob([this.response], {'type': 'audio/mpeg'});
         saveAs(blob, name);
+        hide_progress_bar();
     }
     xhr.open('GET', url, true);
     xhr.send(null);
@@ -100,12 +135,18 @@ function process(element){
         if (element.classList.contains('kz-vk-audio__finished')) return false;
 
         if ((xhr.readyState === 4) && (xhr.status === 200)){
+            //var fragment = document.createDocumentFragment();
             var DOM_kz__wrapper = document.createElement('div');
             DOM_kz__wrapper.classList.add('kz-vk-audio__wrapper');
-            var DOM_kz__btn = document.createElement('div');
-            DOM_kz__btn.classList.add('kz-vk-audio__btn');
 
-            DOM_kz__wrapper.appendChild(DOM_kz__btn);
+            DOM_kz__wrapper.innerHTML =
+                '<div class="kz-vk-audio__btn"></div>' +
+                '<div class="kz-vk-audio__progress kz-hidden">' +
+                    '<div class="kz-vk-audio__progress-filling"></div>'+
+                '</div>';
+
+            var DOM_kz__btn = DOM_kz__wrapper.querySelector('.kz-vk-audio__btn');
+
 /*
             DOM_kz__btn.setAttribute('onmouseover',
                 'Audio.rowActive(this, "Скачать", [9, 5, 0]);');
@@ -114,7 +155,7 @@ function process(element){
 */
             DOM_kz__btn.addEventListener('click', function(event){
                 stopEvent(event);
-                save(url, artist + ' — ' + title + '.mp3');
+                save(url, artist + ' — ' + title + '.mp3', DOM_kz__wrapper);
             }, false)
 
             size = this.getResponseHeader('Content-Length');
