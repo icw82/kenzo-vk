6330724b3a46be721d521eb9fb3ac79d9bdd37d1
unknown <icw82@yandex.ru>
2014-04-11 23:05:30 +0600

    rounding bit rates

--- audio/script.js
+++ audio/script.js
@@ -193,6 +193,15 @@ function process(element){
             size = this.getResponseHeader('Content-Length');
             kbps = Math.floor(size * 8 / duration / 1000);
 
+            if ((kbps >= 288) && (kbps < 352)) kbps = 320; else
+            if ((kbps >= 224) && (kbps < 288)) kbps = 256; else
+            if ((kbps >= 176) && (kbps < 224)) kbps = 192; else
+            if ((kbps >= 144) && (kbps < 176)) kbps = 160; else
+            if ((kbps >= 112) && (kbps < 144)) kbps = 128; else
+            if ((kbps >= 80 ) && (kbps < 112)) kbps = 96; else
+            if ((kbps >= 48 ) && (kbps < 80 )) kbps = 64; else
+            if ((kbps >= 20 ) && (kbps < 48 )) kbps = 32;
+
             var
                 DOM_duration = element.querySelector('.duration'),
                 DOM_actions = element.querySelector('.actions');
@@ -207,8 +216,6 @@ function process(element){
                 DOM_kz__btn.classList.add('kz-vk-audio__bitrate--196');
             else if (kbps > 124)
                 DOM_kz__btn.classList.add('kz-vk-audio__bitrate--128');
-            else if (kbps > 90)
-                DOM_kz__btn.classList.add('kz-vk-audio__bitrate--96');
             else
                 DOM_kz__btn.classList.add('kz-vk-audio__bitrate--crap');
 
