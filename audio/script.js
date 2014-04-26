0bedb6d9ddd579bfbe49ed48fa6fefcde588f4df
unknown <icw82@yandex.ru>
2014-04-26 20:59:54 +0600

    0.6.3

--- audio/script.js
+++ audio/script.js
@@ -19,6 +19,135 @@ function stopEvent(event){
     return false;
 }
 
+function calc_bitrate_classic(size, duration){
+    // Its mutherfucking classic
+    var kbps = Math.floor(size * 8 / duration / 1000);
+
+    if ((kbps >= 288)) kbps = 320; else
+    if ((kbps >= 224) && (kbps < 288)) kbps = 256; else
+    if ((kbps >= 176) && (kbps < 224)) kbps = 192; else
+    if ((kbps >= 144) && (kbps < 176)) kbps = 160; else
+    if ((kbps >= 112) && (kbps < 144)) kbps = 128; else
+    if ((kbps >= 80 ) && (kbps < 112)) kbps = 96; else
+    if ((kbps >= 48 ) && (kbps < 80 )) kbps = 64; else
+    if ((kbps >= 20 ) && (kbps < 48 )) kbps = 32;
+
+    return kbps;
+}
+
+function get_mp3_first_frame_info(url, offset, callback){
+/*
+    var
+        xhr = new XMLHttpRequest(),
+        data = {};
+
+    xhr.onreadystatechange = function(){
+        if ((xhr.readyState === 4) && (xhr.status === 206)){
+
+            console.log(this.getResponseHeader('Content-Range'));
+
+            var buffer = this.response;
+            var int = new Int8Array(buffer);
+            console.log(int);
+        }
+    }
+
+    xhr.open('GET', url, true);
+    xhr.setRequestHeader('Range', 'bytes=' + offset + '-' + (offset + 4));
+    xhr.responseType = 'arraybuffer';
+    xhr.send(null);
+
+    //console.log('get_mp3_first_frame_info');
+*/
+    callback();
+}
+
+
+function get_mp3_info(url, callback){
+    if (typeof callback != 'function'){
+        console.warn('KZVK: get_mp3_info: callback не функция');
+        return false;
+    }
+
+    var
+        xhr = new XMLHttpRequest(),
+        data = {
+            'available': true
+        };
+
+    xhr.onreadystatechange = function(){
+        if (xhr.readyState !== 4) return false;
+
+        if (xhr.status === 206){
+            if (this.getResponseHeader('Content-Type') != 'audio/mpeg'){
+                console.log('KZVK: get_mp3_info:', 'не «audio/mpeg»');
+                callback(data);
+                return false;
+            }
+
+            var buffer = this.response;
+
+            // Размер файла
+            data.size = (function(range){
+                var out = range.match(/\d+?$/);
+                if (out && out[0])
+                    return out[0];
+                else
+                    return false;
+            })(this.getResponseHeader('Content-Range'));
+
+            if (!data.size){
+                callback(data);
+                return false;
+            }
+
+            // Версия тега
+            data.tag_version = (function(buffer){
+                var identifier = new Int8Array(buffer, 0, 3);
+                identifier = String.fromCharCode(identifier[0], identifier[1], identifier[2]);
+
+                if (identifier == 'ID3'){
+                    var version = new Int8Array(buffer, 3, 1);
+                    return 'ID3v2.' + version[0];
+                } else {
+                    //console.log('KZVK: get_mp3_info:', 'not ID3v2');
+                    return false;
+                }
+            })(buffer);
+
+            if (!data.tag_version){
+                callback(data);
+                return false;
+            }
+
+            // Длина тега
+            data.tag_length = (function(buffer){
+                var length = new Int8Array(buffer, 6, 4);
+
+                return length[3] & 0x7f
+                    | ((length[2] & 0x7f) << 7)
+                    | ((length[1] & 0x7f) << 14)
+                    | ((length[0] & 0x7f) << 21);
+            })(buffer);
+
+            // Первый фрейм
+            get_mp3_first_frame_info(url, data.tag_length, function(){
+
+                callback(data);
+            });
+        } else {
+            data.available = false;
+            callback(data);
+        }
+    }
+
+    xhr.open('GET', url, true);
+    xhr.setRequestHeader('Range', 'bytes=0-9');
+    xhr.responseType = 'arraybuffer';
+    xhr.send(null);
+}
+
+
 function save(url, name, element){
     (name) || (name = 'kenzo-vk-audio.mp3');
 
@@ -138,19 +267,6 @@ function init(){
 */
 };
 
-function roundBitrate(kbps){
-    if ((kbps >= 288)) kbps = 320; else
-    if ((kbps >= 224) && (kbps < 288)) kbps = 256; else
-    if ((kbps >= 176) && (kbps < 224)) kbps = 192; else
-    if ((kbps >= 144) && (kbps < 176)) kbps = 160; else
-    if ((kbps >= 112) && (kbps < 144)) kbps = 128; else
-    if ((kbps >= 80 ) && (kbps < 112)) kbps = 96; else
-    if ((kbps >= 48 ) && (kbps < 80 )) kbps = 64; else
-    if ((kbps >= 20 ) && (kbps < 48 )) kbps = 32;
-
-    return kbps;
-};
-
 function process(element){
     var type;
 
@@ -172,7 +288,6 @@ function process(element){
     if (!type) return false;
 
     var
-        xhr = new XMLHttpRequest(),
         id = element.querySelector('a:first-child').getAttribute('name'),
         info = element.querySelector('#audio_info' + id).value.split(','),
         url = info[0],
@@ -197,40 +312,80 @@ function process(element){
     artist = artist.replace(/^s+|\s+$/g, '');
     title = title.replace(/^s+|\s+$/g, '');
 
-    function createButton(kbps){
-        //var fragment = document.createDocumentFragment();
+    function validate_data(data){
+        if (typeof data !== 'object'){
+            console.warn('KZVK: createButton: неправильный формат данных');
+            data = {};
+            //return;
+        }
+
+        if (!('bitrate' in data)){
+            if ('size' in data){
+                if ('tag_version' in data && (
+                    data.tag_version == 'ID3v2.2' ||
+                    data.tag_version == 'ID3v2.3' ||
+                    data.tag_version == 'ID3v2.4'
+                )){
+                    data.bitrate =
+                        calc_bitrate_classic(data.size - data.tag_length - 10, duration);
+                } else
+                    data.bitrate = calc_bitrate_classic(data.size, duration);
+            } else
+                data.bitrate = false;
+        }
+
+        return data;
+    }
+
+    function createButton(data){
+        if (element.classList.contains('kz-vk-audio__finished')) return false;
+
         var DOM_kz__wrapper = document.createElement('div');
         DOM_kz__wrapper.classList.add('kz-vk-audio__wrapper');
 
         DOM_kz__wrapper.innerHTML =
-            '<div class="kz-vk-audio__carousel kz-bitrate">' +
+            '<div class="kz-vk-audio__carousel">' +
                 '<div class="kz-vk-audio__carousel__item kz-bitrate"></div>' +
                 '<div class="kz-vk-audio__carousel__item kz-progress">' +
                     '<div class="kz-vk-audio__progress-filling"></div>' +
                 '</div>' +
+                '<div class="kz-vk-audio__carousel__item kz-unavailable"></div>' +
             '</div>';
 
         var
-            DOM_kz__carousel = DOM_kz__wrapper.querySelector('.kz-vk-audio__carousel'),
-            DOM_kz__bitrate = DOM_kz__wrapper.querySelector('.kz-vk-audio__carousel__item.kz-bitrate');
+            DOM_kz__carousel = DOM_kz__wrapper
+                .querySelector('.kz-vk-audio__carousel'),
+            DOM_kz__bitrate = DOM_kz__wrapper
+                .querySelector('.kz-vk-audio__carousel__item.kz-bitrate'),
+            DOM_kz__unavailable = DOM_kz__wrapper
+                .querySelector('.kz-vk-audio__carousel__item.kz-unavailable');
 
-        DOM_kz__bitrate.addEventListener('click', function(event){
-            stopEvent(event);
-            save(url, artist + ' — ' + title + '.mp3', DOM_kz__wrapper);
-        }, false)
+        if (data.available){
+            DOM_kz__carousel.classList.add('kz-bitrate');
 
-        DOM_kz__bitrate.setAttribute('data-kbps', kbps);
+            DOM_kz__bitrate.addEventListener('click', function(event){
+                stopEvent(event);
+                save(url, artist + ' — ' + title + '.mp3', DOM_kz__wrapper);
+            }, false)
 
-        if (kbps >= 288)
-            DOM_kz__carousel.classList.add('kz-vk-audio__bitrate--320');
-        else if (kbps >= 224)
-            DOM_kz__carousel.classList.add('kz-vk-audio__bitrate--256');
-        else if (kbps >= 176)
-            DOM_kz__carousel.classList.add('kz-vk-audio__bitrate--196');
-        else if (kbps >= 112)
-            DOM_kz__carousel.classList.add('kz-vk-audio__bitrate--128');
-        else
-            DOM_kz__carousel.classList.add('kz-vk-audio__bitrate--crap');
+            DOM_kz__bitrate.setAttribute('data-bitrate', data.bitrate || '??');
+
+            if (data.bitrate >= 288)
+                DOM_kz__carousel.classList.add('kz-vk-audio__bitrate--320');
+            else if (data.bitrate >= 224)
+                DOM_kz__carousel.classList.add('kz-vk-audio__bitrate--256');
+            else if (data.bitrate >= 176)
+                DOM_kz__carousel.classList.add('kz-vk-audio__bitrate--196');
+            else if (data.bitrate >= 112)
+                DOM_kz__carousel.classList.add('kz-vk-audio__bitrate--128');
+            else
+                DOM_kz__carousel.classList.add('kz-vk-audio__bitrate--crap');
+
+        } else {
+            DOM_kz__carousel.classList.add('kz-unavailable');
+            DOM_kz__unavailable.addEventListener('click', stopEvent, false);
+
+        }
 
         if (DOM_play.nextSibling)
             DOM_play.parentElement.insertBefore(DOM_kz__wrapper, DOM_play.nextSibling);
@@ -240,33 +395,19 @@ function process(element){
         element.classList.add('kz-vk-audio__finished');
     }
 
-    function getBitrate(callback){
-        xhr.onreadystatechange = function(){
-            if (element.classList.contains('kz-vk-audio__finished')) return false;
+    if (!url || url == ""){
+        createButton({
+            'available': false
+        });
 
-            if ((xhr.readyState === 4) && (xhr.status === 200)){
-                var
-                    size = this.getResponseHeader('Content-Length'),
-                    kbps = roundBitrate(Math.floor(size * 8 / duration / 1000));
-
-                if (typeof callback == 'function')
-                    callback(kbps);
-                else
-                    console.warn('getBitrate: callback не функция');
-            }
-        }
-
-        xhr.open('HEAD', url, true);
-        xhr.send(null);
-
-        console.log('KZVK:', 'xhr: ', url);
-    };
+        return false;
+    }
 
 /*
-    getBitrate(function(kbps){
-        createButton(kbps);
+    get_mp3_info(url, function(data){
+        createButton(validate_data(data));
     });
-*/
+//*/
 
     (function(){
         var
@@ -306,17 +447,23 @@ function process(element){
 
             request.onsuccess = function(event){
                 if (event.target.result){
-                    createButton(event.target.result.bitrate);
+                    var data = {
+                        'available': true,
+                        'bitrate': event.target.result.bitrate
+                    }
+
+                    createButton(data);
                 } else {
-                    getBitrate(function(kbps){
-                        createButton(kbps);
+                    get_mp3_info(url, function(data){
+                        var valid_data = validate_data(data);
+                        createButton(valid_data);
 
                         connect(function(db){
                             var request = db.transaction([storeName], 'readwrite')
                                 .objectStore(storeName)
                                 .add({
                                     'id': id,
-                                    'bitrate': kbps
+                                    'bitrate': valid_data.bitrate
                                 });
 
                             request.onsuccess = function(){
@@ -324,7 +471,7 @@ function process(element){
                             }
 
                             request.onerror = function(){
-                                console.log('KZVK:', 'add.onerror:', event);
+                                console.warn('KZVK: cache-add:', event);
                                 db.close();
                             }
                         });
@@ -336,14 +483,14 @@ function process(element){
             request.onerror = function(){
                 console.log('KZVK:', 'connect.onerror:', event);
 
-                getBitrate(function(kbps){
-                    createButton(kbps);
+                get_mp3_info(url, function(data){
+                    createButton(validate_data(data));
                 });
             }
         });
 
     })();
-
+//*/
 }
 
 if (document.readyState === 'complete'){
