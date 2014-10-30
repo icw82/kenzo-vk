5090b11f071c3b53a7af47edc00b49468aea1ec9
unknown <icw82@yandex.ru>
2014-10-30 11:34:40 +0500

    stable without audio

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    es = require('event-stream'),
+    del = require('del'),
+    concat = require('gulp-concat'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify'),
+    sourcemaps = require('gulp-sourcemaps');
+
+
+gulp.task('static', function(){
+    return gulp.src('./static/**/*.*')
+        .pipe(gulp.dest('build'))
+});
+
+gulp.task('scripts', function(){
+    var paths = [
+        './scripts/kenzo-set.js',
+        './scripts/kenzo-get-buffer.js',
+        './scripts/defaults.js',
+        './scripts/*.js'
+    ];
+    return gulp.src(paths)
+        .pipe(sourcemaps.init())
+        .pipe(concat('scripts.js'))
+        //.pipe(gulp.dest(paths.build.abs.scripts))
+        //.pipe(sourcemaps.init())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify().on("error", gutil.log))
+        .pipe(sourcemaps.write('/'))
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('watch', function(){
+    gulp.watch(['./static/**/*.*'], ['static']);
+    gulp.watch(['./scripts/*.js'], ['scripts']);
+});
+
+gulp.task('clean', function(callback){
+    del(['build'], callback);
+});
+
+gulp.task('build', ['static', 'scripts']);
+gulp.task('default', ['build', 'watch']);
---
+++ scripts/defaults.js
@@ -0,0 +1,18 @@
+var default_options = {
+    'audio__cache': true,
+    'audio__simplified': false,
+    'audio__vbr': false,
+    'audio__separator': '—',
+    'video__direct_links': false,
+    'trash__lsb__ad': true,
+    'trash__lsb__fr': true,
+    'trash__newsads': true,
+    'trash__group_recom': true
+};
+
+var default_globals = {
+    'audio': {
+        'now_playing': null,
+        'ids': []
+    }
+};
