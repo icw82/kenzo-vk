3d5de6d6418fdb34a5c6bad94b011adac30ed4eb
unknown <icw82@yandex.ru>
2014-11-13 00:15:38 +0500

    1.0.0 RC3

--- scripts/defaults.js
+++ scripts/defaults.js
@@ -1,9 +1,9 @@
 var default_options = {
     'audio__cache': true,
     'audio__simplified': false,
-    'audio__vbr': false,
+    'audio__vbr': true,
     'audio__separator': '—',
-    'video__direct_links': false,
+    'video__direct_links': true,
     'trash__lsb__ad': true,
     'trash__lsb__fr': true,
     'trash__newsads': true,
