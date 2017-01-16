a38c4d69b5f67daf0409528c23defd4e9b0bcbbb
icw82 <icw82@yandex.ru>
2017-01-16 18:39:05 +0500

    update

--- sources/ext/modules/trash/submodules/ads_in_group/main.js
+++ sources/ext/modules/trash/submodules/ads_in_group/main.js
@@ -1,8 +1,14 @@
 sub.init__content = () => {
     if (ext.mode === 2016) {
         core.events.on_mutation.addListener(() => {
-            if (mod.options.ads_in_group)
-                sub.mod.drop('#ads_in_group');
+            if (mod.options.ads_in_group) {
+                //let area = ext.dom.content ? ext.dom.content : document;
+
+                each ('.wall_module .wall_marked_as_ads', target => {
+                    let block = kk.find_ancestor(target, '.page_block');
+                    mod.drop(block);
+                });
+            }
         });
     }
 }
